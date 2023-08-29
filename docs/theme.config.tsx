ebe3732b921b259f3edec5242a3b19eb7537bb94
宋言商 <133555627+SONGYANSHANG@users.noreply.github.com>
2023-08-29 08:07:49 +0800

    Update theme.config.tsx

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -36,11 +36,11 @@ const logo = (
 );
 
 //设置文档的存储库URL。它用于生成“编辑此页面”链接和“反馈”链接
-const config: DocsThemeConfig = {
+//const config: DocsThemeConfig = {
   //设置页眉项目图标的URL
-  project: {
-    link: "https://github.com/SONGYANSHANG/shong-docs",
-  },
+//    project: {
+//      link: "https://github.com/SONGYANSHANG/shong-docs",
+//  },
 
   //设置存储库文档地址，用于生成“编辑此页面”链接和“反馈”链接
   docsRepositoryBase: "https://github.com/SONGYANSHANG/shong-docs/tree/main/docs",
@@ -76,7 +76,7 @@ const config: DocsThemeConfig = {
           content="width=device-width, initial-scale=1.0"
         />{" "}
         // 设置视口元数据，确保网站在移动设备上正确显示。
-        <meta httpEquiv="Content-Language" content="en" /> // 设置页面语言。
+        <meta httpEquiv="Content-Language" content="zh-CN" /> // 设置页面语言。
         <meta name="description" content="宋言商的知识库" /> //
         设置网站描述，用于 SEO。
         <meta name="og:description" content="宋言商的知识库" /> //设置 Open
@@ -131,15 +131,15 @@ const config: DocsThemeConfig = {
   // },
 
   // 用于在页面上添加一个指向 GitHub 上对应页面的编辑链接
-  editLink: {
-    text: "Edit this page on GitHub →",
-  },
+  //editLink: {
+  //  text: "Edit this page on GitHub →",
+  //},
 
   // 用于在页面上添加一个反馈按钮，可以让用户提供反馈信息
-  feedback: {
-    content: "Question? Give us feedback →",
-    labels: "feedback",
-  },
+  //feedback: {
+  //  content: "Question? Give us feedback →",
+  //  labels: "feedback",
+  //},
 
   // 用于配置侧边栏的样式和行为，包括标题组件的设置、默认折叠级别的设置以及是否显示切换侧边栏的按钮
   sidebar: {
@@ -163,7 +163,9 @@ const config: DocsThemeConfig = {
         <a href="https://shong.cc" target="_blank">
           宋言商的工作室
         </a>
-        .
+        蜀ICP备2022015674号-2
+        </a>
+        川公网安备51010602001791号
       </span>
     ),
   },
